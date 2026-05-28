0b51890e138e232c64a1ef8cea6a8a0b6945d82d
fridis <fridi@fridi.de>
2026-05-28 12:48:14 +0200

    bpf: stack allocate `struct event`instead of using `bpf_map_lookup_elemen`
    Not sure why I used the complex map lookup here.

--- src/bpf/feeze_recorder.bpf.c
+++ src/bpf/feeze_recorder.bpf.c
@@ -37,14 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 char LICENSE[] SEC("license") = "GPL";
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__uint(max_entries, 1);
-	__type(key, int);
-	__type(value, struct event);
-} heap SEC(".maps");
-
-
 /* BPF ringbuf map */
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
@@ -58,6 +50,8 @@ struct {
 uint64_t count = 0;
 
 
+
+
 /**
  * sched_switch is a thread switch on a given CPUs. This typically does not
  * directly switch between two arbitray threads, but seems to always switch
@@ -66,61 +60,56 @@ uint64_t count = 0;
 SEC("tracepoint/sched/sched_switch")
 int on_task_switch(struct trace_event_raw_sched_switch *ctx)
 {
-   struct event *e;
-   int zero = 0;
-   e = bpf_map_lookup_elem(&heap, &zero);
-   if (e)
+   struct event es;
+   if (ctx)
      {
-       if (ctx)
+       es.event_kind = RB_EVENT_SCHED_SWITCH;
+       es.old_pid = ctx->prev_pid;
+       es.old_pri = ctx->prev_prio;
+       es.old_name[0] = ctx->prev_comm[0];
+       es.old_name[1] = ctx->prev_comm[1];
+       es.old_name[2] = ctx->prev_comm[2];
+       es.old_name[3] = ctx->prev_comm[3];
+       es.old_name[4] = ctx->prev_comm[4];
+       es.old_name[5] = ctx->prev_comm[5];
+       es.old_name[6] = ctx->prev_comm[6];
+       es.old_name[7] = ctx->prev_comm[7];
+       es.old_name[8] = ctx->prev_comm[8];
+       es.old_name[9] = ctx->prev_comm[9];
+       es.old_name[10] = ctx->prev_comm[10];
+       es.old_name[11] = ctx->prev_comm[11];
+       es.old_name[12] = ctx->prev_comm[12];
+       es.old_name[13] = ctx->prev_comm[13];
+       es.old_name[14] = ctx->prev_comm[14];
+       es.old_name[15] = ctx->prev_comm[15];
+       es.new_pid = ctx->next_pid;
+       es.new_pri = ctx->next_prio;
+       es.comm[0] = ctx->next_comm[0];
+       es.comm[1] = ctx->next_comm[1];
+       es.comm[2] = ctx->next_comm[2];
+       es.comm[3] = ctx->next_comm[3];
+       es.comm[4] = ctx->next_comm[4];
+       es.comm[5] = ctx->next_comm[5];
+       es.comm[6] = ctx->next_comm[6];
+       es.comm[7] = ctx->next_comm[7];
+       es.comm[8] = ctx->next_comm[8];
+       es.comm[9] = ctx->next_comm[9];
+       es.comm[10] = ctx->next_comm[10];
+       es.comm[11] = ctx->next_comm[11];
+       es.comm[12] = ctx->next_comm[12];
+       es.comm[13] = ctx->next_comm[13];
+       es.comm[14] = ctx->next_comm[14];
+       es.comm[15] = ctx->next_comm[15];
+       es.cpu_id = bpf_get_smp_processor_id();
+       es.ns = bpf_ktime_get_ns();
+       es.count = __sync_fetch_and_add(&count, 1);
+       if (bpf_ringbuf_output(&rb, &es, sizeof(es), 0)==0)
          {
-           e->event_kind = RB_EVENT_SCHED_SWITCH;
-           e->old_pid = ctx->prev_pid;
-           e->old_pri = ctx->prev_prio;
-           e->old_name[0] = ctx->prev_comm[0];
-           e->old_name[1] = ctx->prev_comm[1];
-           e->old_name[2] = ctx->prev_comm[2];
-           e->old_name[3] = ctx->prev_comm[3];
-           e->old_name[4] = ctx->prev_comm[4];
-           e->old_name[5] = ctx->prev_comm[5];
-           e->old_name[6] = ctx->prev_comm[6];
-           e->old_name[7] = ctx->prev_comm[7];
-           e->old_name[8] = ctx->prev_comm[8];
-           e->old_name[9] = ctx->prev_comm[9];
-           e->old_name[10] = ctx->prev_comm[10];
-           e->old_name[11] = ctx->prev_comm[11];
-           e->old_name[12] = ctx->prev_comm[12];
-           e->old_name[13] = ctx->prev_comm[13];
-           e->old_name[14] = ctx->prev_comm[14];
-           e->old_name[15] = ctx->prev_comm[15];
-           e->new_pid = ctx->next_pid;
-           e->new_pri = ctx->next_prio;
-           e->comm[0] = ctx->next_comm[0];
-           e->comm[1] = ctx->next_comm[1];
-           e->comm[2] = ctx->next_comm[2];
-           e->comm[3] = ctx->next_comm[3];
-           e->comm[4] = ctx->next_comm[4];
-           e->comm[5] = ctx->next_comm[5];
-           e->comm[6] = ctx->next_comm[6];
-           e->comm[7] = ctx->next_comm[7];
-           e->comm[8] = ctx->next_comm[8];
-           e->comm[9] = ctx->next_comm[9];
-           e->comm[10] = ctx->next_comm[10];
-           e->comm[11] = ctx->next_comm[11];
-           e->comm[12] = ctx->next_comm[12];
-           e->comm[13] = ctx->next_comm[13];
-           e->comm[14] = ctx->next_comm[14];
-           e->comm[15] = ctx->next_comm[15];
-           e->cpu_id = bpf_get_smp_processor_id();
-           e->ns = bpf_ktime_get_ns();
-           e->count = __sync_fetch_and_add(&count, 1);
-           if (bpf_ringbuf_output(&rb, e, sizeof(*e), 0)==0)
-             {
-               // ok
-             }
-           else
-             {
-               // error
-             }
+           // ok
+         }
+       else
+         {
+           // error
          }
      }
 
@@ -135,53 +124,48 @@ SEC("tracepoint/sched/sched_waking")
 int on_task_waking(struct trace_event_raw_sched_wakeup_template *ctx)
 {
   int kind = RB_EVENT_SCHED_WAKING;
-  struct event *e;
-  int zero = 0;
-  e = bpf_map_lookup_elem(&heap, &zero);
-  if (e)
+  struct event es;
+  if (ctx)
     {
-      if (ctx)
+      es.event_kind = kind;
+
+      /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
+
+         es.old_pid = ctx->ent.pid;
+
+         so instead, using `bpf_get_current_pid_tgid`:
+
+      */
+      es.old_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
+
+      es.new_pid = ctx->pid;
+      es.new_pri = ctx->prio;
+      es.comm[0] = ctx->comm[0];
+      es.comm[1] = ctx->comm[1];
+      es.comm[2] = ctx->comm[2];
+      es.comm[3] = ctx->comm[3];
+      es.comm[4] = ctx->comm[4];
+      es.comm[5] = ctx->comm[5];
+      es.comm[6] = ctx->comm[6];
+      es.comm[7] = ctx->comm[7];
+      es.comm[8] = ctx->comm[8];
+      es.comm[9] = ctx->comm[9];
+      es.comm[10] = ctx->comm[10];
+      es.comm[11] = ctx->comm[11];
+      es.comm[12] = ctx->comm[12];
+      es.comm[13] = ctx->comm[13];
+      es.comm[14] = ctx->comm[14];
+      es.comm[15] = ctx->comm[15];
+      es.cpu_id = ctx->target_cpu;
+      es.ns = bpf_ktime_get_ns();
+      es.count = __sync_fetch_and_add(&count, 1);
+      if (bpf_ringbuf_output(&rb, &es, sizeof(es), 0)==0)
         {
-          e->event_kind = kind;
-
-          /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
-
-          e->old_pid = ctx->ent.pid;
-
-             so instead, using `bpf_get_current_pid_tgid`:
-
-          */
-          e->old_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
-
-          e->new_pid = ctx->pid;
-          e->new_pri = ctx->prio;
-          e->comm[0] = ctx->comm[0];
-          e->comm[1] = ctx->comm[1];
-          e->comm[2] = ctx->comm[2];
-          e->comm[3] = ctx->comm[3];
-          e->comm[4] = ctx->comm[4];
-          e->comm[5] = ctx->comm[5];
-          e->comm[6] = ctx->comm[6];
-          e->comm[7] = ctx->comm[7];
-          e->comm[8] = ctx->comm[8];
-          e->comm[9] = ctx->comm[9];
-          e->comm[10] = ctx->comm[10];
-          e->comm[11] = ctx->comm[11];
-          e->comm[12] = ctx->comm[12];
-          e->comm[13] = ctx->comm[13];
-          e->comm[14] = ctx->comm[14];
-          e->comm[15] = ctx->comm[15];
-          e->cpu_id = ctx->target_cpu;
-          e->ns = bpf_ktime_get_ns();
-          e->count = __sync_fetch_and_add(&count, 1);
-          if (bpf_ringbuf_output(&rb, e, sizeof(*e), 0)==0)
-            {
-              // ok
-            }
-          else
-            {
-              // error
-            }
+          // ok
+        }
+      else
+        {
+          // error
         }
     }
   return 0;
@@ -196,53 +180,48 @@ SEC("tracepoint/sched/sched_wakeup")
 int on_task_wakeup(struct trace_event_raw_sched_wakeup_template *ctx)
 {
   int kind = RB_EVENT_SCHED_WAKEUP;
-  struct event *e;
-  int zero = 0;
-  e = bpf_map_lookup_elem(&heap, &zero);
-  if (e)
+  struct event es;
+  if (ctx)
     {
-      if (ctx)
+      es.event_kind = kind;
+
+      /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
+
+         es.old_pid = ctx->ent.pid;
+
+         so instead, using `bpf_get_current_pid_tgid`:
+
+      */
+      es.old_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
+
+      es.new_pid = ctx->pid;
+      es.new_pri = ctx->prio;
+      es.comm[0] = ctx->comm[0];
+      es.comm[1] = ctx->comm[1];
+      es.comm[2] = ctx->comm[2];
+      es.comm[3] = ctx->comm[3];
+      es.comm[4] = ctx->comm[4];
+      es.comm[5] = ctx->comm[5];
+      es.comm[6] = ctx->comm[6];
+      es.comm[7] = ctx->comm[7];
+      es.comm[8] = ctx->comm[8];
+      es.comm[9] = ctx->comm[9];
+      es.comm[10] = ctx->comm[10];
+      es.comm[11] = ctx->comm[11];
+      es.comm[12] = ctx->comm[12];
+      es.comm[13] = ctx->comm[13];
+      es.comm[14] = ctx->comm[14];
+      es.comm[15] = ctx->comm[15];
+      es.cpu_id = ctx->target_cpu;
+      es.ns = bpf_ktime_get_ns();
+      es.count = __sync_fetch_and_add(&count, 1);
+      if (bpf_ringbuf_output(&rb, &es, sizeof(es), 0)==0)
         {
-          e->event_kind = kind;
-
-          /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
-
-          e->old_pid = ctx->ent.pid;
-
-             so instead, using `bpf_get_current_pid_tgid`:
-
-          */
-          e->old_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
-
-          e->new_pid = ctx->pid;
-          e->new_pri = ctx->prio;
-          e->comm[0] = ctx->comm[0];
-          e->comm[1] = ctx->comm[1];
-          e->comm[2] = ctx->comm[2];
-          e->comm[3] = ctx->comm[3];
-          e->comm[4] = ctx->comm[4];
-          e->comm[5] = ctx->comm[5];
-          e->comm[6] = ctx->comm[6];
-          e->comm[7] = ctx->comm[7];
-          e->comm[8] = ctx->comm[8];
-          e->comm[9] = ctx->comm[9];
-          e->comm[10] = ctx->comm[10];
-          e->comm[11] = ctx->comm[11];
-          e->comm[12] = ctx->comm[12];
-          e->comm[13] = ctx->comm[13];
-          e->comm[14] = ctx->comm[14];
-          e->comm[15] = ctx->comm[15];
-          e->cpu_id = ctx->target_cpu;
-          e->ns = bpf_ktime_get_ns();
-          e->count = __sync_fetch_and_add(&count, 1);
-          if (bpf_ringbuf_output(&rb, e, sizeof(*e), 0)==0)
-            {
-              // ok
-            }
-          else
-            {
-              // error
-            }
+          // ok
+        }
+      else
+        {
+          // error
         }
     }
   return 0;
@@ -253,84 +232,79 @@ SEC("usdt")
 int handle_fuzion_probe(struct pt_regs *ctx)
 {
   int kind = RB_EVENT_FUZION_USER;
-  struct event *e;
-  int zero = 0;
-  e = bpf_map_lookup_elem(&heap, &zero);
-  if (e)
+  struct event es;
+  if (ctx)
     {
-      if (ctx)
+      es.event_kind = kind;
+
+      /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
+
+         es.old_pid = ctx->ent.pid;
+
+         so instead, using `bpf_get_current_pid_tgid`:
+
+      */
+      es.new_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
+
+      int argcnt = bpf_usdt_arg_cnt(ctx);
+      long lres;
+      int argres = bpf_usdt_arg(ctx, 0, &lres);
+      es.new_pri = (int) lres;   // col
+      if (argcnt == 5)
         {
-          e->event_kind = kind;
-
-          /* not sure why this does not work, produces `invalid bpf_context access off=4 size=4`
-
-          e->old_pid = ctx->ent.pid;
-
-             so instead, using `bpf_get_current_pid_tgid`:
-
-          */
-          e->new_pid = (pid_t) bpf_get_current_pid_tgid()&0xffffffff;
-
-          int argcnt = bpf_usdt_arg_cnt(ctx);
-          long lres;
-          int argres = bpf_usdt_arg(ctx, 0, &lres);
-          e->new_pri = (int) lres;   // col
-          if (argcnt == 5)
-            {
-              long str0, str1, str2, str3;
-              int argres0 = bpf_usdt_arg(ctx, 1, &str0);
-              int argres1 = bpf_usdt_arg(ctx, 2, &str1);
-              int argres2 = bpf_usdt_arg(ctx, 3, &str2);
-              int argres3 = bpf_usdt_arg(ctx, 4, &str3);
-              e->comm[ 0]     = (char) ((str0 >>  0) & 0xff);
-              e->comm[ 1]     = (char) ((str0 >>  8) & 0xff);
-              e->comm[ 2]     = (char) ((str0 >> 16) & 0xff);
-              e->comm[ 3]     = (char) ((str0 >> 24) & 0xff);
-              e->comm[ 4]     = (char) ((str0 >> 32) & 0xff);
-              e->comm[ 5]     = (char) ((str0 >> 40) & 0xff);
-              e->comm[ 6]     = (char) ((str0 >> 48) & 0xff);
-              e->comm[ 7]     = (char) ((str0 >> 56) & 0xff);
-              e->comm[ 8]     = (char) ((str1 >>  0) & 0xff);
-              e->comm[ 9]     = (char) ((str1 >>  8) & 0xff);
-              e->comm[10]     = (char) ((str1 >> 16) & 0xff);
-              e->comm[11]     = (char) ((str1 >> 24) & 0xff);
-              e->comm[12]     = (char) ((str1 >> 32) & 0xff);
-              e->comm[13]     = (char) ((str1 >> 40) & 0xff);
-              e->comm[14]     = (char) ((str1 >> 48) & 0xff);
-              e->comm[15]     = (char) ((str1 >> 56) & 0xff);
-              e->old_name[ 0] = (char) ((str2 >>  0) & 0xff);
-              e->old_name[ 1] = (char) ((str2 >>  8) & 0xff);
-              e->old_name[ 2] = (char) ((str2 >> 16) & 0xff);
-              e->old_name[ 3] = (char) ((str2 >> 24) & 0xff);
-              e->old_name[ 4] = (char) ((str2 >> 32) & 0xff);
-              e->old_name[ 5] = (char) ((str2 >> 40) & 0xff);
-              e->old_name[ 6] = (char) ((str2 >> 48) & 0xff);
-              e->old_name[ 7] = (char) ((str2 >> 56) & 0xff);
-              e->old_name[ 8] = (char) ((str3 >>  0) & 0xff);
-              e->old_name[ 9] = (char) ((str3 >>  8) & 0xff);
-              e->old_name[10] = (char) ((str3 >> 16) & 0xff);
-              e->old_name[11] = (char) ((str3 >> 24) & 0xff);
-              e->old_name[12] = (char) ((str3 >> 32) & 0xff);
-              e->old_name[13] = (char) ((str3 >> 40) & 0xff);
-              e->old_name[14] = (char) ((str3 >> 48) & 0xff);
-              e->old_name[15] = (char) ((str3 >> 56) & 0xff);
-              e->event_kind = kind;
-            }
-          else
-            {
-              e->comm[0] = 'X';
-              e->comm[1] = 0;
-            }
-          e->ns = bpf_ktime_get_ns();
-          e->count = 3;
-          if (bpf_ringbuf_output(&rb, e, sizeof(*e), 0)==0)
-            {
-              // ok
-            }
-          else
-            {
-              // error
-            }
+          long str0, str1, str2, str3;
+          int argres0 = bpf_usdt_arg(ctx, 1, &str0);
+          int argres1 = bpf_usdt_arg(ctx, 2, &str1);
+          int argres2 = bpf_usdt_arg(ctx, 3, &str2);
+          int argres3 = bpf_usdt_arg(ctx, 4, &str3);
+          es.comm[ 0]     = (char) ((str0 >>  0) & 0xff);
+          es.comm[ 1]     = (char) ((str0 >>  8) & 0xff);
+          es.comm[ 2]     = (char) ((str0 >> 16) & 0xff);
+          es.comm[ 3]     = (char) ((str0 >> 24) & 0xff);
+          es.comm[ 4]     = (char) ((str0 >> 32) & 0xff);
+          es.comm[ 5]     = (char) ((str0 >> 40) & 0xff);
+          es.comm[ 6]     = (char) ((str0 >> 48) & 0xff);
+          es.comm[ 7]     = (char) ((str0 >> 56) & 0xff);
+          es.comm[ 8]     = (char) ((str1 >>  0) & 0xff);
+          es.comm[ 9]     = (char) ((str1 >>  8) & 0xff);
+          es.comm[10]     = (char) ((str1 >> 16) & 0xff);
+          es.comm[11]     = (char) ((str1 >> 24) & 0xff);
+          es.comm[12]     = (char) ((str1 >> 32) & 0xff);
+          es.comm[13]     = (char) ((str1 >> 40) & 0xff);
+          es.comm[14]     = (char) ((str1 >> 48) & 0xff);
+          es.comm[15]     = (char) ((str1 >> 56) & 0xff);
+          es.old_name[ 0] = (char) ((str2 >>  0) & 0xff);
+          es.old_name[ 1] = (char) ((str2 >>  8) & 0xff);
+          es.old_name[ 2] = (char) ((str2 >> 16) & 0xff);
+          es.old_name[ 3] = (char) ((str2 >> 24) & 0xff);
+          es.old_name[ 4] = (char) ((str2 >> 32) & 0xff);
+          es.old_name[ 5] = (char) ((str2 >> 40) & 0xff);
+          es.old_name[ 6] = (char) ((str2 >> 48) & 0xff);
+          es.old_name[ 7] = (char) ((str2 >> 56) & 0xff);
+          es.old_name[ 8] = (char) ((str3 >>  0) & 0xff);
+          es.old_name[ 9] = (char) ((str3 >>  8) & 0xff);
+          es.old_name[10] = (char) ((str3 >> 16) & 0xff);
+          es.old_name[11] = (char) ((str3 >> 24) & 0xff);
+          es.old_name[12] = (char) ((str3 >> 32) & 0xff);
+          es.old_name[13] = (char) ((str3 >> 40) & 0xff);
+          es.old_name[14] = (char) ((str3 >> 48) & 0xff);
+          es.old_name[15] = (char) ((str3 >> 56) & 0xff);
+          es.event_kind = kind;
+        }
+      else
+        {
+          es.comm[0] = 'X';
+          es.comm[1] = 0;
+        }
+      es.ns = bpf_ktime_get_ns();
+      es.count = 3; // NYI: remove?
+      if (bpf_ringbuf_output(&rb, &es, sizeof(es), 0)==0)
+        {
+          // ok
+        }
+      else
+        {
+          // error
         }
     }
   return 0;
